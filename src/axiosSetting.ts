07ca159bd5b16816eb5e28c4b5bb580e3eb36cde
okw98jn <j.ookawa.0417@gmail.com>
2024-05-25 22:55:17 +0900

    envから取得

--- src/axiosSetting.ts
+++ src/axiosSetting.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:8099/api/",
+  baseURL: process.env.API_URL,
   headers: {
     "Content-Type": "application/json",
   },
