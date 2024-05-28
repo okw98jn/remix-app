38d3687e1f1b424d9c7820fcbd33988b05d0b7d1
okw98jn <j.ookawa.0417@gmail.com>
2024-05-28 22:36:59 +0900

    文言修正

--- src/app/components/error/Unauthorized.tsx
+++ src/app/components/error/Unauthorized.tsx
@@ -14,10 +14,10 @@ const Unauthorized = () => {
           You do not have permission to access this page. Please check your
           credentials and try again.
         </div>
-        {/* フロント側だけログイン状態になることがないよう一応ログアウトしておく */}
+        {/* クライアント側だけログイン状態になることがないよう一応ログアウトしておく */}
         <Form method="post" action="logout">
           <button className="mt-4 inline-flex h-10 items-center justify-center rounded-md bg-gray-900 px-6 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300">
-            ログアウト
+            go to login page
           </button>
         </Form>
       </div>
