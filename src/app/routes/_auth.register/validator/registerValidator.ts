bb58750788b2aa6443c899103cab620f08499fc4
okw98jn <j.ookawa.0417@gmail.com>
2024-06-01 22:14:10 +0900

    バリデーション追加

--- src/app/routes/_auth.register/validator/registerValidator.ts
+++ src/app/routes/_auth.register/validator/registerValidator.ts
@@ -6,10 +6,12 @@ const registerValidator = withZod(
     email: z
       .string()
       .min(1, { message: "メールアドレスを入力してください" })
+      .max(255, { message: "メールアドレスは255文字以内で入力してください" })
       .email({ message: "メールアドレスを正しい形式で入力してください" }),
     password: z
       .string()
-      .min(4, { message: "パスワードは4文字以上入力してください" }),
+      .min(4, { message: "パスワードは4文字以上入力してください" })
+      .max(255, { message: "パスワードは255文字以内で入力してください" }),
   })
 );
 
